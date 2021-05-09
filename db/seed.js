4dc464d52c691b780b4b384539fbe8393c5b24f2
Maxwell Montegary <maxwellmontegary@gmail.com>
2021-05-09 10:53:11 -0700

    Part 3 Finished

--- db/seed.js
+++ db/seed.js
@@ -9,7 +9,11 @@ const {
   createPost,
   updatePost,
   getAllPosts,
-  getPostsByUser
+  getPostsByUser,
+  createTags,
+  addTagsToPost,
+  getPostsByTagName
+  
 } = require('./index');
 
 
@@ -20,6 +24,8 @@ async function dropTables() {
 
     // have to make sure to drop in correct order
     await client.query(`
+      DROP TABLE IF EXISTS post_tags;
+      DROP TABLE IF EXISTS tags;
       DROP TABLE IF EXISTS posts;
       DROP TABLE IF EXISTS users;
     `);
@@ -30,6 +36,8 @@ async function dropTables() {
     throw error;
   }
 }
+
+
 
 async function createTables() {
   try {
@@ -50,6 +58,15 @@ async function createTables() {
           title VARCHAR(255) NOT NULL,
           content TEXT NOT NULL,
           active BOOLEAN DEFAULT true
+        );
+        CREATE TABLE tags (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL
+        );
+        CREATE TABLE post_tags (
+          "postId" INTEGER REFERENCES posts(id),
+          "tagId" INTEGER REFERENCES tags(id),
+          UNIQUE ("postId","tagId")
         );
       `);
 
@@ -74,11 +91,31 @@ async function createInitialUsers() {
     throw error;
   }
 }
+// async function createInitialTags() {
+//   try {
+//     console.log("Starting to create tags...");
+
+//     const [happy, sad, inspo, catman] = await createTags([
+//       '#happy', 
+//       '#worst-day-ever', 
+//       '#youcandoanything',
+//       '#catmandoeverything'
+//     ]);
+
+//     const [postOne, postTwo, postThree] = await getAllPosts();
+
+//     await addTagsToPost(postOne.id, [happy, inspo]);
+//     await addTagsToPost(postTwo.id, [sad, inspo]);
+//     await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+//     console.log("Finished creating tags!");
+//   } catch (error) {
+//     console.log("Error creating tags!");
+//     throw error;
+//   }
+// }
 
 
-
-
-// then modify rebuildDB to call our new function
 async function rebuildDB() {
   try {
     client.connect();
@@ -86,8 +123,10 @@ async function rebuildDB() {
     await dropTables();
     await createTables();
     await createInitialUsers();
-    
+    await createInitialPosts();
+    await createInitialTags(); // new
   } catch (error) {
+    console.log("Error during rebuildDB")
     throw error;
   }
 }
@@ -108,6 +147,8 @@ async function testDB() {
     });
     console.log("Result:", updateUserResult);
 
+
+
     console.log("Calling getAllPosts");
     const posts = await getAllPosts();
     console.log("Result:", posts);
@@ -122,6 +163,17 @@ async function testDB() {
     console.log("Calling getUserById with 1");
     const albert = await getUserById(1);
     console.log("Result:", albert);
+
+
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"]
+    });
+    console.log("Result:", updatePostTagsResult);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log("Finished database tests!");
   } catch (error) {
@@ -138,19 +190,22 @@ async function createInitialPosts() {
     await createPost({
       authorId: albert.id,
       title: "First Post",
-      content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+      content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+      tags: ["#happy", "#youcandoanything"]
     });
 
     await createPost({
       authorId: sandra.id,
       title: "How does this work?",
-      content: "Seriously, does this even do anything?"
+      content: "Seriously, does this even do anything?",
+      tags: ["#happy", "#worst-day-ever"]
     });
 
     await createPost({
       authorId: glamgal.id,
       title: "Living the Glam Life",
-      content: "Do you even? I swear that half of you are posing."
+      content: "Do you even? I swear that half of you are posing.",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
     });
     console.log("Finished creating posts!");
   } catch (error) {
@@ -172,7 +227,6 @@ async function rebuildDB() {
     throw error;
   }
 }
-
 
 
 rebuildDB()
